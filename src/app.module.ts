b3aa861a27f3298bd0eaa9d9b18550c96a0471d9
jf <jfgm299@gmail.com>
2026-05-20 22:21:58 +0200

    feat: add due processing job

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AccountsModule } from "./accounts/accounts.module";
 import { CategoriesModule } from "./categories/categories.module";
 import { CoreModule } from "./core/core.module";
 import { HealthController } from "./health.controller";
+import { InternalJobsModule } from "./internal-jobs/internal-jobs.module";
 import { RecurringTransactionsModule } from "./recurring-transactions/recurring-transactions.module";
 import { ScheduledTransactionsModule } from "./scheduled-transactions/scheduled-transactions.module";
 import { TransactionsModule } from "./transactions/transactions.module";
 
 @Module({
-  imports: [CoreModule, AccountsModule, CategoriesModule, TransactionsModule, RecurringTransactionsModule, ScheduledTransactionsModule],
+  imports: [CoreModule, AccountsModule, CategoriesModule, TransactionsModule, RecurringTransactionsModule, ScheduledTransactionsModule, InternalJobsModule],
   controllers: [HealthController]
 })
 export class AppModule {}
